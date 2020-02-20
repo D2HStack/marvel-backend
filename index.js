3bc1b8a2a3e26c679518c2d27337da18bbe9abed
Hoang <D2HStack@users.noreply.github.com>
2020-02-20 04:10:26 +0100

    api characters and comics

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const express = require("express");
 const expressFormidable = require("express-formidable");
 const cors = require("cors");
-const dotenv = require("dotenv");
 require("dotenv").config();
-
-// request utility
-const request = require("./utility/request");
+const uid2 = require("uid2");
+const md5 = require("md5");
+const axios = require("axios");
 
 // set up server
 const app = express();
@@ -17,15 +16,78 @@ app.get("/", (req, res) => {
   res.json("Hello ! Welcome to Marvel !");
 });
 
+// request characters with offset and limit
 app.get("/characters/", async (req, res) => {
   try {
     const { offset, limit } = req.query;
-    const response = await request(
-      "http://gateway.marvel.com/v1/public/characters",
-      "get",
-      offset,
-      limit
+    const ts = uid2(64);
+    const hash = md5(
+      ts + process.env.MARVEL_SECRET + process.env.MARVEL_PUBLIC
     );
+
+    const response = await axios({
+      url: "http://gateway.marvel.com/v1/public/characters",
+      method: "get",
+      params: {
+        apikey: process.env.MARVEL_PUBLIC,
+        ts,
+        hash,
+        offset,
+        limit
+      }
+    });
+    res.json(response.data.data);
+  } catch (error) {
+    res.status(error.code).json({ error: { message: error.status } });
+  }
+});
+
+// request character with id
+app.get("/characters/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const ts = uid2(64);
+    const hash = md5(
+      ts + process.env.MARVEL_SECRET + process.env.MARVEL_PUBLIC
+    );
+
+    const response = await axios({
+      url: `http://gateway.marvel.com/v1/public/characters/${id}`,
+      method: "get",
+      params: {
+        apikey: process.env.MARVEL_PUBLIC,
+        ts,
+        hash
+      }
+    });
+    res.json(response.data.data);
+  } catch (error) {
+    res.status(error.code).json({ error: { message: error.status } });
+  }
+});
+
+// request comics  with offset and limit adn related to character with id
+app.get("/characters/:id/comics", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { offset, limit } = req.query;
+
+    const ts = uid2(64);
+    const hash = md5(
+      ts + process.env.MARVEL_SECRET + process.env.MARVEL_PUBLIC
+    );
+
+    const response = await axios({
+      url: `http://gateway.marvel.com/v1/public/characters/${id}/comics`,
+      method: "get",
+      params: {
+        apikey: process.env.MARVEL_PUBLIC,
+        ts,
+        hash,
+        offset,
+        limit
+      }
+    });
     res.json(response.data.data);
   } catch (error) {
     res.status(error.code).json({ error: { message: error.status } });
